1aa7d392da9176b4fbba68fa432b250b4aba683e
Kezzoul Massili <massili.kezzoul@etu.umontpellier.fr>
2021-11-19 13:01:50 +0100

    Pretraitement + Construction d'un model

---
+++ src/tools/pretraitement.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def normalize_dataset(x_train, y_train):
+    # Scale images to the [0, 1] range
+    x_train = x_train.astype("float32") / np.amax(x_train)
+    # Flatten the images.
+    #x_train = x_train.reshape((-1, len(x_train[0]) * len(x_train[0][0])))
+    # One hot encoding
+    unique_y = np.sort(np.unique(y_train))
+    y_train = np.array(list(map(lambda x:  [1 if x == k else 0 for k in unique_y], y_train)))
+
+    return x_train , y_train
