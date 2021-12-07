6e87470f7d56bfc878d01f7fbbcca6d7df53f5e9
Kezzoul Massili <massy.kezzoul@gmail.com>
2021-12-07 21:47:28 +0100

    data augmentation

--- src/tools/pretraitement.py
+++ src/tools/pretraitement.py
@@ -3,6 +3,12 @@ import numpy as np
 def normalize_dataset(x_train, y_train):
     # Scale images to the [0, 1] range
     x_train = x_train.astype("float32") / np.amax(x_train)
+
+    # mean and standart deviation
+    mean = np.mean(x_train)
+    std = np.std(x_train)
+    x_train = (x_train - mean) / std
+
     # Flatten the images.
     #x_train = x_train.reshape((-1, len(x_train[0]) * len(x_train[0][0])))
     # One hot encoding
